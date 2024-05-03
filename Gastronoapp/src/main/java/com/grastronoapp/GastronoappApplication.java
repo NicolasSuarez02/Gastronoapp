625fcac07a679e1f9b934bba756b6919af19bf5e
NicolasSuarez02 <107655003+NicolasSuarez02@users.noreply.github.com>
2024-05-03 13:14:40 -0300

    Add files via upload

---
+++ Gastronoapp/src/main/java/com/grastronoapp/GastronoappApplication.java
@@ -0,0 +1,13 @@
+package com.grastronoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastronoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastronoappApplication.class, args);
+	}
+
+}
---
+++ Gastronoapp/src/test/java/com/grastronoapp/GastronoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grastronoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastronoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
